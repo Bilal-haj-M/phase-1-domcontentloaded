e7c0f864a4f1f6f5dc0ef771f8c203afce562331
Bilal-hajM <bblal1003@gmail.com>
2023-04-24 03:36:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    callDom()
+  });
+
+  function callDom () {
+    document.querySelector("#text");
+    text.textContent = "This is really cool!"
+  }
+
+  
